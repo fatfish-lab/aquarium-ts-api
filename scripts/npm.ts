2a9b6a2f830b0fd9adfca07ad5f5a0d1970e5cd9
Yann Moriaud <yann@fatfi.sh>
2025-09-02 17:29:11 +0200

    feat: Use deno.json version for npm package

--- scripts/npm.ts
+++ scripts/npm.ts
@@ -1,7 +1,9 @@
 import { build, emptyDir } from "https://deno.land/x/dnt@0.37.0/mod.ts";
+import deno from "../deno.json" with { type: "json" }
 
 const newTag = new Deno.Command('git', {
-    args: ['tag', '-a', `${Deno.args[0]}`, '-m', `v${Deno.args[0]}`]})
+  args: ['tag', '-a', `${deno.version}`, '-m', `v${deno.version}`]
+})
 newTag.outputSync()
 
 await emptyDir("./npm");
@@ -10,27 +12,28 @@ Deno.copyFileSync("LICENSE", "npm/LICENSE")
 Deno.copyFileSync("README.md", "npm/README.md")
 
 await build({
-    entryPoints: ["./src/index.ts"],
-    outDir: "./npm",
-    typeCheck: false,
-    compilerOptions: {
-        lib: ['DOM']
+  entryPoints: ["./src/index.ts"],
+  outDir: "./npm",
+  typeCheck: false,
+  compilerOptions: {
+    lib: ['DOM']
+  },
+  shims: {},
+  package: {
+    name: "@fatfish-lab/aquarium-ts-api",
+    version: deno.version,
+    description: "Aquarium API typescript package",
+    license: "GPL-3.0",
+    types: "./src/index.d.ts",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/fatfish-lab/aquarium-ts-api.git",
     },
-    shims: {},
-    package: {
-        name: "@fatfish-lab/aquarium-ts-api",
-        version: Deno.args[0],
-        description: "Aquarium API typescript package",
-        license: "GPL-3.0",
-        repository: {
-            type: "git",
-            url: "git+https://github.com/fatfish-lab/aquarium-ts-api.git",
-        },
-        bugs: {
-            url: "https://github.com/fatfish-lab/aquarium-ts-api/issues",
-        },
-        publishConfig: {
-            "registry": "https://npm.pkg.github.com"
-        },
-    }
+    bugs: {
+      url: "https://github.com/fatfish-lab/aquarium-ts-api/issues",
+    },
+    publishConfig: {
+      "registry": "https://npm.pkg.github.com"
+    },
+  }
 });
