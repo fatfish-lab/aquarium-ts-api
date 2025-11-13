fb73627e16ece1068badba885a089927ef83ddac
Yann Moriaud <yann@fatfi.sh>
2025-11-13 17:08:23 +0100

    fix(context): Fix context type

--- src/types/context.ts
+++ src/types/context.ts
@@ -3,7 +3,7 @@ import type { Edge } from "./edge.ts";
 
 export interface Context<I extends string | undefined, E extends string | undefined, Ip extends boolean = false, Ep extends boolean = false> {
   item: Item<I, Ip>
-  edges: Edge<E, Ep>[]
+  edge: Edge<E, Ep>
 }
 
 export interface Path {
